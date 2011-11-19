c5dfe79e6cdd17151a8ffd8ed313bf82c11714ed
PigSnowball <kvilliers@gmail.com>
2011-11-20 06:30:43 +1100

    added my geolocation service :)

---
+++ code/src/com/porknbunny/friendcompass/Friend.java
@@ -0,0 +1,11 @@
+package com.porknbunny.friendcompass;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pigsnowball
+ * Date: 20/11/2011
+ * Time: 04:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class Friend {
+}
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+$link = mysql_connect('localhost:3307', 'root', 'root');
+
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+//echo 'Connected successfully\n';
+mysql_select_db('fc', $link);
+
+
+//http://localhost:8888/?user=kvilliers&lat=-37.23&long=142.34&biz=h3ffh093h&friend=leis2leis
+
+$sql = "INSERT INTO `fc`.`loc` (`userid`, `lat`, `long`, `businessid`, `friend`,".
+"`time`) VALUES ('".$_GET["user"]."', '".$_GET["lat"]."', '".$_GET["long"].
+"', '".$_GET["biz"]."', '".$_GET["friend"]."', CURRENT_TIMESTAMP)";
+
+$result = mysql_query($sql,$link);
+
+
+//echo $result;
+
+$output = "SELECT  `userid` ,  `lat` ,  `long` ,  `businessid` ,
+`friend` , UNIX_TIMESTAMP(MAX(  `time` )) as `time` 
+FROM  `loc` 
+GROUP BY `userid`";
+
+//echo $output."\n";
+
+$result = mysql_query($output,$link);
+
+if (!$result) {
+    $message  = 'Invalid query: ' . mysql_error() . "\n";
+    $message .= 'Whole query: ' . $query;
+    die($message);
+}
+
+$arrayToBeEncoded = array();
+$i = 0;
+while ($row = mysql_fetch_assoc($result)) {
+	$array = array();
+
+	$array['userid'] = $row['userid'];
+	$array['lat'] = $row['lat'];
+	$array['long'] = $row['long'];
+	$array['businessid'] = $row['businessid'];
+	$array['friend'] = $row['friend'];
+	$array['time'] = $row['time'];
+	
+	/*
+    echo $row['userid'];
+    echo $row['lat'];
+    echo $row['long'];
+    echo $row['businessid'];
+    echo $row['friend'];
+    echo $row['time'];
+    
+    echo "\n";
+    echo $array;
+    */
+    $arrayToBeEncoded[$i++] = $array;
+}
+
+echo json_encode($arrayToBeEncoded);
+mysql_close($link);
+?>
