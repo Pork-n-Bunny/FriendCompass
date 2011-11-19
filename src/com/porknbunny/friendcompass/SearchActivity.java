fd55c89918fe3aa7222a557fe8c7b8686ed6ad3d
PigSnowball <kvilliers@gmail.com>
2011-11-19 14:22:45 +1100

    added download method. probably need to refector it to somewhere...

--- src/com/porknbunny/friendcompass/SearchActivity.java
+++ src/com/porknbunny/friendcompass/SearchActivity.java
@@ -7,8 +7,14 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class SearchActivity extends FragmentActivity {
     private static final String TAG = "FriendCompass.SearchActivity";
+    private static final int TEMP_BUFF_SIZE = 16384;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +35,26 @@ public class SearchActivity extends FragmentActivity {
             Log.e(TAG, "Failed to load meta-data, NullPointer: " + e.getMessage());
         }
         return "";
+    }
+
+
+    private BufferedInputStream getUrl(String url) {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setReadTimeout(5000);
+            InputStream inputStream = (InputStream) connection.getContent();
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, TEMP_BUFF_SIZE);
+
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
+                return bufferedInputStream;
+            }
+            Log.w(TAG, "Incorrect response for " + url + " : " + connection.getResponseCode());
+        } catch (Exception e) {
+            e.printStackTrace();
+            //Log.w(TAG, e.getMessage());
+            Log.w(TAG, "Could not get " + url);
+        }
     }
 
 }
