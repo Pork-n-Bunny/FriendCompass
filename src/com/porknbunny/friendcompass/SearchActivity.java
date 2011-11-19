4e6de0dc1a2decabab5fdb58c576a1df7ccd868e
PigSnowball <kvilliers@gmail.com>
2011-11-19 14:32:53 +1100

    added a TextWatcher so can do away with the Go button on search

--- src/com/porknbunny/friendcompass/SearchActivity.java
+++ src/com/porknbunny/friendcompass/SearchActivity.java
@@ -4,22 +4,27 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-import android.widget.Toast;
+import android.widget.EditText;
 
 import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class SearchActivity extends FragmentActivity {
+public class SearchActivity extends FragmentActivity implements TextWatcher {
     private static final String TAG = "FriendCompass.SearchActivity";
     private static final int TEMP_BUFF_SIZE = 16384;
+    private EditText searchField;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Toast.makeText(this, getMetaData("SAPI_KEY"), Toast.LENGTH_SHORT).show();
+
+        searchField = (EditText) findViewById(R.id.searchField);
+        searchField.addTextChangedListener(this);
     }
 
     private String getMetaData(String key) {
@@ -55,6 +60,24 @@ public class SearchActivity extends FragmentActivity {
             //Log.w(TAG, e.getMessage());
             Log.w(TAG, "Could not get " + url);
         }
+        return null;
     }
 
+
+    //---- TextWatcher ----
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        //do search
+
+    }
 }
